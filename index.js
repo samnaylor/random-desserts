41e13e4c34ca412defa5ddc5d84d5eb479f42546
Nicholas Martin <nicholas.martin@tharsus.co.uk>
2020-08-28 15:20:07 +0100

    basic functionality

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const seedrandom = require('seedrandom');
+
 const desserts = [
-    ''
+    'cake',
+    'ice-cream',
+    'gateau'
 ];
+
+export function getRandomDessertsName() {
+    const rng = seedrandom();
+    const index = Math.floor(rng) * desserts.length();
+    return desserts[index];
+}
